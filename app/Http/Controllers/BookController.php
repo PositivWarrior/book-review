9d42638b0b6324addb7c2b0a12149ee0a1470f57
Kacper Margol <kacpermargol@gmail.com>
2025-04-07 11:47:23 +0200

    Implement caching for book index retrieval to improve performance

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -27,7 +27,10 @@ class BookController extends Controller
             default => $books,
         };
 
-        $books = $books->get();
+        // $books = $books->get();
+
+        $cacheKey = 'books:' . $filter . ':' . $title;
+        $books = cache()->remember($cacheKey, 3600, fn() => $books->get());
 
         return view('books.index', ['books' => $books]);
     }
